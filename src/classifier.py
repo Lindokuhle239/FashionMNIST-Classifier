3185f6fb6b8f1308e3affcfa6f27fbee73799de6
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-05-04 19:39:22 +0000

    Defined the image processing pipeline

--- src/classifier.py
+++ src/classifier.py
@@ -9,3 +9,8 @@ import torchvision
 #defining class names corresponding to the FashionMNIST labels
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
+#defining image processing pipeline
+transform = transforms.Compose([
+    transforms.ToTensor(), #converts PIL image to tensor
+    transforms.Normalize((0.5), (0.5)) #normalize to [-1,1] range
+])
