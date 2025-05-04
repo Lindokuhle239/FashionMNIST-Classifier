3e8b92166c046627755c612d386fd2c285a7a9bc
Lindokuhle Mdlalose <mdllin028@myuct.ac.za>
2025-05-04 19:35:49 +0000

    Defined class names

--- src/classifier.py
+++ src/classifier.py
@@ -5,3 +5,7 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms, io
 import pathlib
 import torchvision
+
+#defining class names corresponding to the FashionMNIST labels
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
